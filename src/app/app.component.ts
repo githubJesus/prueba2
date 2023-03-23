049b7782a605cf4731049d4d1e193587ad8184c1
Jesus Garcia <jesus.garcialop20@gmail.com>
2023-03-23 16:34:39 +0100

    CommitGrafico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {ApexChart} from 'ng-apexcharts'
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   title = 'prueba2';
+  chartOptions: any = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+      type: 'pie',
+      height: 350,
+    },
+    labels: ['Apple', 'Mango', 'Banana', 'Orange', 'Watermelon'],
+  };
+  
+  chart: ApexChart = {
+    type: 'pie',
+    height: 350,
+  };
+  
+  ngOnInit() {
+    // asigna la propiedad chartOptions.chart a la propiedad chart
+    this.chart = this.chartOptions.chart;
+  }
+     
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgApexchartsModule } from "ng-apexcharts";
+
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgApexchartsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
+
+
